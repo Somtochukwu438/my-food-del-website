16d3443499c9b852939dd9194694599d495341cc
Somtochukwu438 <143945662+Somtochukwu438@users.noreply.github.com>
2024-11-21 18:33:12 -0600

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -100,7 +100,7 @@ import axios from "axios";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-    const url = "http://localhost:4000"
+    const url = "https://gourmet-express-server.onrender.com"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState(localStorage.getItem("token") || "");
